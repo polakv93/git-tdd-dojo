30a5263f8706f8b8441c96ad2c18a9757af364f5
dStankiewicz <dstan1992@gmail.com>
2018-12-15 14:19:01 +0100

    Fix calc

--- Math/Calc.cs
+++ Math/Calc.cs
@@ -6,9 +6,9 @@ namespace Math
     {
         public int Result { get; set; }
 
-        public void SetInitial(int v)
+        public void SetInitial(int? v)
         {
-            Result = v;
+            Result = v ?? 0;
         }
 
         public void Add(int v) {
