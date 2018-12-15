da01ab8d935235a5e54da5fc81cbc0bcb9f33d34
dStankiewicz <dstan1992@gmail.com>
2018-12-15 15:02:36 +0100

    Create tests

---
+++ Math.Tests/CalcTests/calc_exp/calc_exp_test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Math.Tests.CalcTests.calc_exp
+{
+    class calc_exp_test
+    {
+        private calc_exp_fixture _fixture;
+
+        public calc_exp_test()
+        {
+            _fixture = new calc_exp_fixture();
+        }
+
+        [Fact]
+        public void return_value_1__when__exponentiation_to_0()
+        {
+            _fixture.arrange(20);
+
+            _fixture.act(0);
+
+            _fixture.assert_should_be(1);
+
+        }
+
+        [Fact]
+        public void return_same_value__when__exponentiation_to_1()
+        {
+            _fixture.arrange(25);
+
+            _fixture.act(1);
+
+            _fixture.assert_should_be(25);
+        }
+
+        [Fact]
+        public void return_correct_value__when__exponentiation()
+        {
+            _fixture.arrange(3);
+
+            _fixture.act(4);
+
+            _fixture.assert_should_be(81);
+        }
+    }
+}
